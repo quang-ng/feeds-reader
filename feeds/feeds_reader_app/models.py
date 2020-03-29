e940e8cd6e23962be9a783d567348ca216f444cb
quang-sudo <nguyenvanquang247@gmail.com>
2020-03-29 10:33:15 +0700

    Create database

--- feeds/feeds_reader_app/models.py
+++ feeds/feeds_reader_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.TextField()
+    link = models.URLField()
+    category = models.CharField(max_length=1000)
+    comments = models.URLField()
+    pubDate = models.DateTimeField()
