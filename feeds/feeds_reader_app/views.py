6bd84480c16f322d85f951460bb46d5b4e2cd576
quang-sudo <nguyenvanquang247@gmail.com>
2020-03-30 17:09:51 +0700

    Detail page

--- feeds/feeds_reader_app/views.py
+++ feeds/feeds_reader_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.conf import settings
@@ -20,3 +20,7 @@ def index(request):
         channels = paginator.page(paginator.num_pages)
 
     return render(request, 'feeds_reader_app/index.html', { 'channels': channels })
+
+def detail(request, channel_id):
+    channel = get_object_or_404(Channel, pk=channel_id)
+    return render(request, 'feeds_reader_app/detail.html', {'channel': channel})
